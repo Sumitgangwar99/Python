b9aad1bf7fdb43e5dc10d4ef84c21501601c4ddc
Sumit <ut9550@gmail.com>
2023-04-14 02:56:54 +0530

    how to use comment and escape

---
+++ escape,comment.py
@@ -0,0 +1,10 @@
+print("sumit is a good boy\n and ----")
+#print("bye")
+'''print("hello")'''
+print("hello \'world\'")
+print("hello\"world\"")
+#\" \" use for written word in inverted comas
+#\n use to change line 
+#use for comment
+#''' '''and """ """ are also use for comment
+print("bye\n GOOD NIGHT")
