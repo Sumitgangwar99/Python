1d65c8167be231e3efbbb966863e1468be5275a5
Sumit <ut9550@gmail.com>
2023-04-22 22:54:01 +0530

    basic calculator day 8

---
+++ basic calculator.py
@@ -0,0 +1,9 @@
+a=10
+b=12
+print("The value of",a,"+",b,"is:",a+b)
+print("The value of",a,"-",b,"is:",a-b)
+print("The value of",a,"*",b,"is:",a*b)
+print("The value of",a,"/",b,"is:",a/b)
+print("The value of",a,"%",b,"is:",a%b)
+print("The value of",a,"//",b,"is:",a//b)
+print("The value of",a,"**",b,"is:",a**b)
